e21597a6b830e1a975b8b5bc43fdae1e40af0e05
Kaio Fontes <53804110+KaioFontes@users.noreply.github.com>
2020-12-12 15:15:21 -0300

    first commit

---
+++ lamp.js
@@ -0,0 +1,16 @@
+//Settings of the lamp
+
+//function to turn on the lamp
+turnOn = () =>{
+    document.getElementById('image').src = 'img/ligada.jpg';
+}
+
+//Function to turn off the lamp
+turnOff = () =>{
+    document.getElementById('image').src = 'img/desligada.jpg';
+}
+
+//Function to Break the lamp
+lampBreak = () =>{
+    document.getElementById('image').src = 'img/quebrada.jpg';
+}
